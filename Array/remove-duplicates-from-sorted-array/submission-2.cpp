7776578879f2865bec2b2de420007982b3447513
MundaneDragon <158457052+MundaneDragon@users.noreply.github.com>
2026-09-05 14:10:10 +0000

    Sync: Remove Duplicates from Sorted Array (C++)

---
+++ Array/remove-duplicates-from-sorted-array/submission-2.cpp
@@ -0,0 +1,17 @@
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto left = 1;
+
+        for (auto right = 1; right < nums.size(); right++) {
+            if (nums[right-1] != nums[right]) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        
+        return left;
+    }
+};
