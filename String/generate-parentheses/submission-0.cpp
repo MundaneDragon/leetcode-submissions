823d329713277bb7d3086610d1e60c1a216304e5
MundaneDragon <158457052+MundaneDragon@users.noreply.github.com>
2026-09-06 03:03:50 +0000

    Sync: Generate Parentheses (C++)

---
+++ String/generate-parentheses/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        auto result = std::vector<std::string>{};
+        backtrack(0, 0, n, result, "");
+        
+        return result;
+    }
+
+    void backtrack(int openN, int closeN, int index, vector<string>& result, string current) {
+        if ((openN == index) && (closeN == index)) {
+            result.push_back(current);
+            return;
+        }
+
+        if (openN < index) {
+            current.push_back('(');
+            backtrack(openN+1, closeN, index, result, current);
+            current.pop_back();
+        }
+
+        if (closeN < openN) {
+            current.push_back(')');
+            backtrack(openN, closeN+1, index, result, current);
+            current.pop_back();
+        }
+    }
+};
